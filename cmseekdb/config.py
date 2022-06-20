c3121159247153ec3a4ac1c8898537250651c59f
Jack Ou <93158511+zou-godaddy@users.noreply.github.com>
2022-06-20 15:01:02 -0400

    set default directory for reports to the project directory

--- cmseekdb/config.py
+++ cmseekdb/config.py
@@ -3,6 +3,8 @@
 # This is a part of CMSeeK, check the LICENSE file for more information
 # Copyright (c) 2018 - 2020 Tuhinshubhra
 
+import os
+
 # Verbose
 verbose = False
 
@@ -24,5 +26,6 @@ log_dir = ""
 # access_directory contains the path to the directory where reports directory and reports.json files are saved
 # leave it empty to use default dir (cmseek directory if writeaccess else the current directory the user is in)
 # if you want to use a custom path.. enter the full path below
+# access_directory = ""
 
-access_directory = ""
+access_directory = os.path.dirname(os.path.abspath(__file__)).replace('cmseekdb','')[:-1]
